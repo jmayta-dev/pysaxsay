045d931f5c431c75da14ed564de54a7976838149
Jheison J. Mayta C <8401842+jmayta-dev@users.noreply.github.com>
2025-03-08 01:16:42 -0500

    🎉 starting pysaxsay

---
+++ pysaxsay/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for pysaxsay project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pysaxsay.settings')
+
+application = get_asgi_application()
